6306f7f0239b5ac32b394a80936a225b3c06e918
KayJay <keyjay624@gmail.com>
2016-05-18 22:06:26 +0200

    Refactoring

--- compiler.py
+++ compiler.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python
 from scaner import Scaner
-#from parser import Parser
-from par import Parser
+from parser import Parser
 from codeGenerator import CodeGenerator
 import sys
 
@@ -15,12 +14,11 @@ scaner = Scaner()
 parser = Parser()
 codeGen = CodeGenerator()
 
-tokens = scaner.sca(expression)
-#rpn = parser.infixToRPN(sc)
+tokens = scaner.scan(expression)
 tree = parser.constructTree(tokens)
 commands = codeGen.createCommands(tree)
-#command_list = cg.create_commands(rpn);
 cl = ""
+
 for item in commands:
     print(item)
     cl = cl + item + ";"
